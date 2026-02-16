aad91785d28309f71cda3f28a4c7698377be802c
Muzyk0 <muzyk0@proton.me>
2026-02-16 23:41:23 +0300

    chore(db): remove legacy migration files
    Delete obsolete migration files that have already been applied:
    - 000001_initial_schema
    - 000002_add_encrypted_pii_fields
    - 000003_fix_reservation_column_names
    - 000004_add_updated_at_to_reservations
    - 000005_refactor_gift_items_many_to_many

--- backend/internal/app/database/migrations/000003_fix_reservation_column_names.down.sql
+++
@@ -1,4 +0,0 @@
--- Revert reservation column name fixes
-
-ALTER TABLE reservations RENAME COLUMN cancel_reason TO cancelled_reason;
-ALTER TABLE reservations RENAME COLUMN canceled_at TO cancelled_at;
--- backend/internal/app/database/migrations/000004_add_updated_at_to_reservations.down.sql
+++
@@ -1,6 +0,0 @@
--- Remove index on updated_at
-DROP INDEX IF EXISTS idx_reservations_updated_at;
-
--- Remove updated_at column from reservations table
-ALTER TABLE reservations
-DROP COLUMN IF EXISTS updated_at;
